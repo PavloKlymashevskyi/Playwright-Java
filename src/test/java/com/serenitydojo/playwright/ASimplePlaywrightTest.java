8e0167edd787eb1d76c728b3df352da45693b527
PavloKlymashevskyi <klymashevskyi.pavlo@gmail.com>
2026-01-12 12:14:10 -0500

    Configuration browser without An Annotated Playwright Test

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,14 +1,40 @@
 package com.serenitydojo.playwright;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-@UsePlaywright
+import java.util.Arrays;
+
 public class ASimplePlaywrightTest {
+    Playwright playwright;
+    Browser browser;
+    Page page;
+
+    @BeforeEach
+    public void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                            .setHeadless(false)
+                            .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+        );
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    public void teardown() {
+        browser.close();
+        playwright.close();
+    }
+
     @Test
-    public void shouldShowThePageTitle(Page page) {
+    public void shouldShowThePageTitle() {
 
         page.navigate("https://practicesoftwaretesting.com/");
         String title = page.title();
@@ -17,7 +43,7 @@ public class ASimplePlaywrightTest {
     }
 
     @Test
-    public void shouldSearchByKeyword(Page page) {
+    public void shouldSearchByKeyword() {
 
         page.navigate("https://practicesoftwaretesting.com/");
         page.locator("#search-query").fill("Pliers");
