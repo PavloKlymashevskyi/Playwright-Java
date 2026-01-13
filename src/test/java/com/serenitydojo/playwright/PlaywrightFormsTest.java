445cc4ed43e2e79653500fbc0777289a5a653821
PavloKlymashevskyi <klymashevskyi.pavlo@gmail.com>
2026-01-13 13:35:26 -0500

    Testing Mandatory Fields

--- src/test/java/com/serenitydojo/playwright/PlaywrightFormsTest.java
+++ src/test/java/com/serenitydojo/playwright/PlaywrightFormsTest.java
@@ -1,8 +1,11 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.SelectOption;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.net.URISyntaxException;
 import java.nio.file.Path;
@@ -81,6 +84,32 @@ public class PlaywrightFormsTest {
             org.assertj.core.api.Assertions.assertThat(uploadedFile).endsWith("sample-data.txt");
         }
 
+        @DisplayName("mandatory fields")
+        @ParameterizedTest
+        @ValueSource(strings = {"First Name", "Last Name", "Email", "Message"})
+        void mandatoryFields(String fieldName) {
+            var firstNameField = page.getByLabel("First Name");
+            var lastNameField = page.getByLabel("Last Name");
+            var emailNameField = page.getByLabel("Email");
+            var messageField = page.getByLabel("Message");
+            var sendButton = page.getByText("Send");
+
+            // Fill in the field values
+            firstNameField.fill("Sara-Jane");
+            lastNameField.fill("Smith");
+            emailNameField.fill("example@example.com");
+            messageField.fill("Hello World");
+
+            // Clear one of the fields
+            page.getByLabel(fieldName).clear();
+
+            sendButton.click();
+
+            // Check the error message for that field
+            var errorMessage = page.getByRole(AriaRole.ALERT).getByText(fieldName + " is required");
+            assertThat(errorMessage).isVisible();
+
+        }
     }
 
 }
