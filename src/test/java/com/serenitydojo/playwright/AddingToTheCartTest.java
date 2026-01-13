4099afaf3dd87d199d04c3825debf0e5e1c74c11
PavloKlymashevskyi <klymashevskyi.pavlo@gmail.com>
2026-01-13 12:13:46 -0500

    using @UsePlaywright Annotation

--- src/test/java/com/serenitydojo/playwright/AddingToTheCartTest.java
+++ src/test/java/com/serenitydojo/playwright/AddingToTheCartTest.java
@@ -1,6 +1,7 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.junit.UsePlaywright;
 import com.microsoft.playwright.options.AriaRole;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
@@ -10,45 +11,12 @@ import java.util.List;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-
+@UsePlaywright
 public class AddingToTheCartTest {
-    protected static Playwright playwright;
-    protected static Browser browser;
-    protected static BrowserContext browserContext;
-
-    Page page;
-
-    @BeforeAll
-    static void setUpBrowser() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
-                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
-        );
-
-        playwright.selectors().setTestIdAttribute("data-test");
-    }
-
-    @BeforeEach
-    void setUp() {
-        browserContext = browser.newContext();
-        page = browserContext.newPage();
-    }
-
-    @AfterEach
-    void closeContext() {
-        browserContext.close();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        browser.close();
-        playwright.close();
-    }
 
     @DisplayName("Search for pliers")
     @Test
-    void searchForPliers() {
+    void searchForPliers(Page page) {
         page.navigate("https://practicesoftwaretesting.com");
 
         page.getByPlaceholder("Search").fill("Pliers");
