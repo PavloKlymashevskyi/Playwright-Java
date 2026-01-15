cd6f4a5196974e182b15f2ae99ceb01d8aaab2dc
PavloKlymashevskyi <klymashevskyi.pavlo@gmail.com>
2026-01-15 09:27:02 -0500

    demonstration of a simple Page Object

---
+++ src/test/java/com/serenitydojo/playwright/examples/PageObjectTest.java
@@ -0,0 +1,103 @@
+package com.serenitydojo.playwright.examples;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PageObjectTest {
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+
+    Page page;
+
+    @BeforeAll
+    static void setUpBrowser() {
+        playwright = Playwright.create();
+        playwright.selectors().setTestIdAttribute("data-test");
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(true)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+        );
+    }
+
+    @BeforeEach
+    void setUp() {
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        browserContext.close();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+
+    @BeforeEach
+    void openHomePage() {
+        page.navigate("https://practicesoftwaretesting.com");
+    }
+
+    @DisplayName("Without Page Object")
+    @Test
+    void withoutPageObject() {
+        page.waitForResponse("**/products/search?q=tape", () -> {
+            page.getByPlaceholder("Search").fill("tape");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click();
+        });
+
+        List<String> matchingProducts = page.getByTestId("product-name").allInnerTexts();
+        Assertions.assertThat(matchingProducts)
+                .contains("Tape Measure 7.5m", "Measuring Tape", "Tape Measure 5m");
+    }
+
+    @DisplayName("With Page Objects")
+    @Test
+    void withPageObjects() {
+        SearchComponent searchComponent = new SearchComponent(page);
+        ProductList productList = new ProductList(page);
+        searchComponent.searchBy("tape");
+
+        var matchingProducts = productList.getProductNames();
+
+        Assertions.assertThat(matchingProducts).contains("Tape Measure 7.5m", "Measuring Tape", "Tape Measure 5m");
+    }
+
+    class SearchComponent {
+        private final Page page;
+
+        SearchComponent(Page page) {
+            this.page = page;
+        }
+
+        public void searchBy(String keyword) {
+            page.waitForResponse("**/products/search?q=tape", () -> {
+                page.getByPlaceholder("Search").fill(keyword);
+                page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click();
+            });
+        }
+    }
+
+    class ProductList {
+        private final Page page;
+
+        ProductList(Page page) {
+            this.page = page;
+        }
+
+
+        public List<String> getProductNames() {
+            return page.getByTestId("product-name").allInnerTexts();
+        }
+    }
+
+}
